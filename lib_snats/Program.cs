1ea8cd50de6e90f533adc0e56e53d46e9b21e420
DerJulien <julian@dicken.de>
2019-05-13 20:41:10 +0200

    Initial Commit

--- lib_snats/Program.cs
+++ lib_snats/Program.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using lib_snats.Genetics;
-
-namespace lib_snats
+﻿namespace lib_snats
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Genes gene = new Genes();
+
         }
     }
 }
